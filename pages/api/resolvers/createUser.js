3a9cdfb0e904970786f3a9b10e9fa9e9bca004d1
jamacio.rocha <jamacio.rocha@danone.com>
2022-10-09 19:45:52 -0300

    added md5 on project

--- pages/api/resolvers/createUser.js
+++ pages/api/resolvers/createUser.js
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client'
+import md5 from 'md5'
 
 const prisma = new PrismaClient();
 
 async function createUser(parent, args, context, info) {
 
-    const pass = args.pass;
+    const pass = md5(args.pass);
 
     const user = await prisma.user.create({
         data: {
