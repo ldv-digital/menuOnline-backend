2be0ca9f34046e4190975461404aedd14019f0a5
jamacio.rocha <jamacio.rocha@danone.com>
2023-02-20 18:58:37 -0300

    solved bug update pass

--- pages/api/resolvers/updateUser.js
+++ pages/api/resolvers/updateUser.js
@@ -8,16 +8,18 @@ async function updateUser(parent, args, context, info) {
     const userId = parseInt(context?.idUserToken);
     if (!userId) return [];
 
-    const pass = md5(args.pass);
+    const passEncrypted = (args?.pass && uemail != undefined ? md5(args.pass) : false);
+    let data = { ...args };
+
+    if (passEncrypted) {
+        data.pass = passEncrypted;
+    }
 
     const user = await prisma.user.update({
         where: {
             id: userId,
         },
-        data: {
-            ...args,
-            pass
-        },
+        data,
     })
 
     return user;
