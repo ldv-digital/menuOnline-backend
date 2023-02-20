1ae98121cdb785ebd375a3a13a6b272d0301ead1
jamacio.rocha <jamacio.rocha@danone.com>
2023-02-20 19:35:24 -0300

    solved bug update pass

--- pages/api/resolvers/updateUser.js
+++ pages/api/resolvers/updateUser.js
@@ -4,15 +4,23 @@ import md5 from 'md5'
 const prisma = new PrismaClient();
 
 async function updateUser(parent, args, context, info) {
-
+    let data = {};
     const userId = parseInt(context?.idUserToken);
     if (!userId) return [];
 
     const passEncrypted = (args?.pass && args?.pass != undefined ? md5(args.pass) : false);
-    let data = { ...args };
 
-    if (passEncrypted) {
-        data.pass = passEncrypted;
+
+    if (args?.pass) {
+        data.pass = md5(args.pass);
+    }
+
+    if (args?.email) {
+        data.email = args.email;
+    }
+
+    if (args?.name) {
+        data.name = args.name;
     }
 
     const user = await prisma.user.update({
